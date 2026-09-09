44deb388ec779f68cea956a31d9c7282df780d4b
umarshah-blip <umarshahmd@gmail.com>
2026-09-09 13:58:52 +0530

    Add files via upload

---
+++ area-of-a-triangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    printf("enter height of the triangle: ");
+    scanf("%d",&a);
+    printf("enter base of the triangle: ");
+    scanf("%d",&b);
+    printf("area of the triangle is:%f",0.5*a*b);
+    
+    return 0;
+    
+}
