d1250e0259972fef49261f09d85299e2e14efc85
umarshah-blip <umarshahmd@gmail.com>
2026-09-09 20:01:41 +0530

    Add files via upload

---
+++ print-the-smaller-number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int a;
+int b;
+printf("enter no.");
+scanf("%d",&a);
+printf("enter no.");
+scanf("%d",&b);
+if (a<b){
+   printf("%d",a);
+}
+else{
+   printf("%d",b);
+}
+
+   return 0;
+}
